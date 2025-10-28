2557cff4eb87a995c6777f7f6b376d9bd335d722
Ahmed Mohamed Lakosha <ahmedlakosha72@gmail.com>
2025-10-28 20:47:04 +0300

    Task8.py

--- Task8.py
+++ Task8.py
@@ -1,5 +1,4 @@
 def is_prime(n):
-    """Check if a number is prime"""
     if n < 2:
         return False
     if n == 2:
@@ -7,7 +6,6 @@ def is_prime(n):
     if n % 2 == 0:
         return False
     
-    # Check divisibility from 3 to sqrt(n)
     for i in range(3, int(n**(1/2)) + 1, 2):
         if n % i == 0:
             return False
@@ -22,4 +20,5 @@ for num in range(2, 101):
         total =sum (primes)
 
 print("Prime numbers between 2 and 100:" , primes)
+
 print("Sum of all prime numbers between 2 and 100,", total)
